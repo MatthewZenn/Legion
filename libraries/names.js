e5f46736741360cf1ef6f83d85682bfd8660170e
Matthew Zenn <spyguy57@gmail.com>
2023-11-21 11:46:49 -0500

    added name generator

---
+++ libraries/names.js
@@ -0,0 +1,79 @@
+var firstname = 'john';
+var lastname = 'smith';
+var age = 0;
+var country = 'asd';
+var gender = '';
+var p = 0;
+
+var countries = ['America', 'India', 'Germany', 'Canada']
+
+document.getElementById("profile").addEventListener('click', () => {
+  identity();
+});
+
+function identity() {
+  document.getElementById('textarea').innerHTML = '';
+  age = Math.floor(Math.random() * (60 - 18 + 1) + 18);
+  p = Math.random();
+
+  if(p >= 0.5) {
+    gender = 'Male';
+  }
+  else {
+    gender = 'Female';
+  }
+
+  country = countries[Math.floor(Math.random()*countries.length)];
+
+  if(country == 'America') {
+    usnames();
+  }
+  else if(country == 'India') {
+    indianames();
+  }
+  else if(country == 'Germany') {
+    germannames();
+  }
+  else {
+    canadanames();
+  }
+
+  lastname = lastnames[Math.floor(Math.random()*lastnames.length)];
+  document.getElementById('textarea').innerHTML = firstname + ' ' + lastname + '\n' + 'gender: ' + gender + '\n' + 'birth: ' + country + '\n' + 'age: ' + age;
+}
+
+function usnames() {
+  if(p >= 0.5) {
+    firstname = males_us[Math.floor(Math.random()*males_us.length)];
+  }
+  else {
+    firstname = females_us[Math.floor(Math.random()*females_us.length)];
+  }
+}
+
+function indianames() {
+  if(p >= 0.5) {
+    firstname = males_india[Math.floor(Math.random()*males_india.length)];
+  }
+  else {
+    firstname = females_india[Math.floor(Math.random()*females_india.length)];
+  }
+}
+
+function germannames() {
+  if(p >= 0.5) {
+    firstname = males_german[Math.floor(Math.random()*males_german.length)];
+  }
+  else {
+    firstname = females_german[Math.floor(Math.random()*females_german.length)];
+  }
+}
+
+function canadanames() {
+  if(p >= 0.5) {
+    firstname = males_canada[Math.floor(Math.random()*males_canada.length)];
+  }
+  else {
+    firstname = females_canada[Math.floor(Math.random()*females_canada.length)];
+  }
+}
